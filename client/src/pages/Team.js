93a8495b5c0ea23e4305818b28d7872982038776
Ron <fybishron@gmail.com>
2020-07-17 23:18:50 +0300

    Complete Teams component functionality
    redircet by id to scecific team page

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
-const Team = () => {
-    return (
-        <div>
-            <h1>Team Page</h1>
-        </div>
-    )
-}
+const Team = ({ match }) => {
+	useEffect(() => {
+		getTeamByid(match.params.id);
+    }, []);
+    
+	const getTeamByid = async id => {
+		try {
+            const res = await axios.get(`/api/teams/${id}`);
+            console.log(res.data)
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	
+	return (
+		<div>
+			<h1>Team Page</h1>
+		</div>
+	);
+};
 
-export default Team
+export default Team;
--- client/src/pages/Teams.js
+++ client/src/pages/Teams.js
@@ -1,9 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import SportsSoccerSharpIcon from '@material-ui/icons/SportsSoccerSharp';
+import SendSharpIcon from '@material-ui/icons/SendSharp';
 import { Link } from 'react-router-dom';
 import Spinner from '../components/Spinner';
 import axios from 'axios';
 
-const Teams = () => {
+const useStyles = makeStyles(theme => ({
+	table: {
+		minWidth: 650,
+	},
+	tabler: { backgroundColor: 'lightblue' },
+}));
+
+const Teams = ({ history }) => {
+	const classes = useStyles();
 	useEffect(() => {
 		getTeams();
 	}, []);
@@ -13,8 +32,6 @@ const Teams = () => {
 	const getTeams = async () => {
 		try {
 			const res = await axios.get('/api/teams');
-            console.log(res.data);
-            console.log("test frmo api",res.data.length)
 			setsTeams(res.data);
 		} catch (error) {
 			console.log(error);
@@ -25,9 +42,44 @@ const Teams = () => {
 		<div>
 			<Link to='/'>back to home page !</Link>
 			<h1>Teams Page</h1>
-            {
-                teams && teams.length ? teams.map((team =><div key={team.name}>{team.name}</div>)):<Spinner/> 
-            }
+			{teams && teams.length ? (
+				<TableContainer component={Paper}>
+					<Table className={classes.table} aria-label='simple table'>
+						<TableHead className={classes.tabler}>
+							<TableRow>
+								<TableCell>Team Name</TableCell>
+								<TableCell align='right'>Founded</TableCell>
+								<TableCell align='right'>Address</TableCell>
+								<TableCell align='right'>Visit Page</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{teams.map(team => (
+								<TableRow key={team.id}>
+									<TableCell component='th' scope='row'>
+										{team.name}
+									</TableCell>
+									<TableCell align='right'>
+										{team.founded}
+									</TableCell>
+									<TableCell align='right'>
+										{team.address}
+									</TableCell>
+									<TableCell >
+										<Link to={`/teams/${team.id}`} style={{textDecoration:'none'}}>
+											<IconButton  color="primary">
+												<SendSharpIcon />
+											</IconButton>
+										</Link>
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</TableContainer>
+			) : (
+				<Spinner />
+			)}
 		</div>
 	);
 };
