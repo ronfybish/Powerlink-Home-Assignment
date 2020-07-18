39a16fc6c422e6836b80bf5e582ed7ec193e41fd
Ron <fybishron@gmail.com>
2020-07-18 23:26:30 +0300

    complete Landing component & prepare for deploy

--- client/src/components/style.js
+++ client/src/components/style.js
@@ -29,3 +29,29 @@ export const useHeaderStyles = makeStyles(theme => ({
 		position: 'relative',
 	},
 }));
+
+export const useLadingStyles = makeStyles(theme => ({
+	mainFeaturedPost: {
+		position: 'relative',
+		backgroundColor: theme.palette.grey[800],
+		color: theme.palette.common.white,
+		backgroundSize: 'cover',
+		backgroundRepeat: 'no-repeat',
+		backgroundPosition: 'center',
+		height:'100vh'
+	},
+	overlay: {
+		position: 'absolute',
+		top: 0,
+		bottom: 0,
+		right: 0,
+		left: 0,
+		backgroundColor: 'rgba(0,0,0,.3)',
+	},
+	headerContent: {
+		position: 'relative',
+	},
+	test:{
+		height:'100vh'
+	}
+}));
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { Grid, Paper, Typography, Button } from '@material-ui/core/';
+import { useLadingStyles } from '../components/style';
 const Landing = () => {
+	const classes = useLadingStyles();
 	return (
-		<div>
-			<h1>Landing Page</h1>
+		<div className={classes.test}>
+			<Paper
+				className={classes.mainFeaturedPost}
+				style={{
+					backgroundImage: `url(${'https://i.pinimg.com/originals/82/a9/cb/82a9cb48d053275f9a8c013dadbe1ffb.jpg'})`,
+				}}
+			>
+				{
+					<img
+						style={{ display: 'none' }}
+						src={
+							'https://i.pinimg.com/originals/82/a9/cb/82a9cb48d053275f9a8c013dadbe1ffb.jpg'
+						}
+					/>
+				}
 
-			<Link to='/teams'>Go To Teams !</Link>
+				<div className={classes.overlay} />
+
+				<Grid container alignItems='center' direction='column' style={{padding:'10rem'}}>
+					<Grid item>
+						<Typography
+							component='h1'
+							variant='h1'
+							color='inherit'
+							className={classes.headerContent}
+						>
+							It's Time To Play
+						</Typography>
+					</Grid>
+					<Grid item>
+						<Link to='/teams'>
+							<Button size='large' variant='contained' color='primary'>
+								Get Started
+							</Button>
+						</Link>
+					</Grid>
+				</Grid>
+			</Paper>
 		</div>
 	);
 };
